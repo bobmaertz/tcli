18cea9b1705e13a76981f7bb7323953c983f9c22
Bob Maertz <r.maertz@gmail.com>
2024-04-21 21:18:33 -0400

    Fixing up command line template

---
+++ cmd/tcli/main.go
@@ -0,0 +1,35 @@
+package main
+
+import (
+    "os"
+    "flag"
+    "github.com/bobmaertz/tcli/internal/version" 
+)
+
+var commands = map[string]func([]string){
+	"version": version.Print,
+}
+
+func init() {
+    //TODO:  Define flags 
+    flag.Parse()
+}
+
+
+func main() {
+    args := flag.Args()
+
+    if len(args) < 1 {
+        usage()        
+    }
+
+    // Take the first argument as the command to run with the second argument as the argument to the command
+    commands[args[0]](args[1:])
+}
+
+
+func usage() {
+    flag.Usage()
+    os.Exit(0)
+}
+
